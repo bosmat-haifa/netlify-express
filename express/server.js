09d622ea34ef9ccc0b0d701a69eb83a88d5fe74f
bosmat-haifa <drdy.business@gmail.com>
2020-09-13 21:18:05 +0300

    f

--- express/server.js
+++ express/server.js
@@ -17,6 +17,8 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/home', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/test', (req, res) => res.sendFile(path.join(__dirname, '../test.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
